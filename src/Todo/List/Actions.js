fad9387b5be1fab099f19954d0fbea1a7e976356
Kaio Brito <kaio.a7x@gmail.com>
2018-08-24 22:31:55 -0300

    Fix previous commit

--- src/Todo/List/Actions.js
+++ src/Todo/List/Actions.js
@@ -1,8 +1,12 @@
 export const actionTypes = {
+  FETCH: "TODO/FETCH",
   TOGGLE: "TODO/TOGGLE"
 };
 
 export const actionCreators = {
+  fetch: () => ({
+    type: actionTypes.FETCH
+  }),
   toggle: todo => ({
     type: actionTypes.TOGGLE,
     payload: todo
